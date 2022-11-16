3a0e841df79b3e03451b5c7269ba6e485a24e532
Sravya_Vaddi <svaddi@albany.edu>
2022-11-16 02:56:19 -0500

    Resolved CORS error

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.get('/add',(req,res)=> {
     console.log(req.query.a,req.query.b )
     res.json({"sum":  parseInt(req.query.a)+parseInt(req.query.b)})
